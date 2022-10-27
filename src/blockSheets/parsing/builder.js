b74e68828b39a08b681f38da5ee0f77b21cc6a03
TJ Bearse <5686806+tjbearse@users.noreply.github.com>
2022-10-27 11:25:52 -0400

    refactor builder switch to functions

--- src/blockSheets/parsing/builder.js
+++ src/blockSheets/parsing/builder.js
@@ -5,9 +5,6 @@ import '../blocks/Math/blocks'
 import '../blocks/Logical/standardBlocks'
 import '../blocks/Text/blocks'
 
-// FIXME magic constant input_value
-const InputValue = 1
-
 export const buildBlocks = (workspace, tree, init=()=>{}) => {
 	let block = null;
 	if (tree === null) {
@@ -15,22 +12,7 @@ export const buildBlocks = (workspace, tree, init=()=>{}) => {
 	}
 	switch (tree.kind) {
 		case 'value':
-			switch (typeof tree.value) {
-				case 'number':
-					block = workspace.newBlock('sheets_number')
-					block.setFieldValue(tree.value, 'NUM')
-					break;
-				case 'string':
-					block = workspace.newBlock('sheets_text')
-					block.setFieldValue(tree.value, 'TEXT')
-					break;
-				case 'boolean':
-					block = workspace.newBlock('sheets_boolean')
-					block.setFieldValue(tree.value? 'TRUE' : 'FALSE', 'BOOL')
-					break;
-				default:
-					throw new Error("unimplemented value type")
-			}
+			block = makeValueBlock(workspace, tree, init);
 			break;
 		case 'range':
 			block = workspace.newBlock('sheets_cell')
@@ -40,93 +22,127 @@ export const buildBlocks = (workspace, tree, init=()=>{}) => {
 			block = makeArrayBlock(workspace, tree, init);
 			break;
 		case 'func':
-			block = workspace.newBlock(`sheets_${tree.name}`)
-			const inputs = block.inputList.filter(i => i.type === InputValue)
-			try {
-				if (tree.args.length > inputs.length) {
-					throw new Error(`too many arguments given (${tree.args.length}) for function ${tree.name} (wanted ${inputs.length})`)
-				}
-				tree.args.forEach((a, i) => {
-					const b2 = buildBlocks(workspace, a, init)
-					if (b2 !== null) {
-						const conn = inputs[i].connection
-						conn.connect(b2.outputConnection)
-					}
-				})
-			} catch(e) {
-				block.dispose()
-				throw e
-			}
-
+			block = makeFuncBlock(workspace, tree, init);
 			break;
 		case 'binaryOp':
-			{ // code block scopes variables to this case
-				let  op, blockName;
-				switch (tree.symb) {
-					// arithmetic
-					case '+': blockName = 'arithmetic'; op = 'ADD'; break;
-					case '-': blockName = 'arithmetic'; op = 'MINUS'; break;
-					case '*': blockName = 'arithmetic'; op = 'MULTIPLY'; break;
-					case '/': blockName = 'arithmetic'; op = 'DIVIDE'; break;
-					case '^': blockName = 'arithmetic'; op = 'POWER'; break;
-					// compare
-					case '<': blockName = 'compare'; op = 'LT'; break;
-					case '<=': blockName = 'compare'; op = 'LTE'; break;
-					case '=': blockName = 'compare'; op = 'EQ'; break;
-					case '<>': blockName = 'compare'; op = 'NEQ'; break;
-					case '>': blockName = 'compare'; op = 'GT'; break;
-					case '>=': blockName = 'compare'; op = 'GTE'; break;
-					// concat TODO
-					case '&': blockName = 'join'; break;
-					default:
-						throw new Error(`unimplemented binary op: ${tree.symb}`)
-				}
-				block = workspace.newBlock(`sheets_${blockName}`)
-				if (op) {
-					block.setFieldValue(op, 'OP')
-				}
-				try {
-					const left = buildBlocks(workspace, tree.left, init)
-					const right = buildBlocks(workspace, tree.right, init)
-					if (left !== null) {
-						const conn = block.getInput('A').connection
-						conn.connect(left.outputConnection)
-					}
-					if (right !== null) {
-						const conn = block.getInput('B').connection
-						conn.connect(right.outputConnection)
-					}
-				} catch(e) {
-					block.dispose()
-					throw e
-				}
-			}
+			block = makeBinOpBlock(workspace, tree, init);
 			break;
 		case 'unaryOp':
-			{ // code block scopes variables to this case
-				let blockName;
-				switch (tree.symb) {
-					case '-': blockName = 'negate'; break;
-					default:
-						throw new Error(`unimplemented unary op: ${tree.symb}`)
-				}
-				block = workspace.newBlock(`sheets_${blockName}`)
-				try {
-					const right = buildBlocks(workspace, tree.right, init)
-					if (right !== null) {
-						const conn = block.getInput('A').connection
-						conn.connect(right.outputConnection)
-					}
-				} catch(e) {
-					block.dispose()
-					throw e
-				}
-			}
+			block = makeUnOpBlock(workspace, tree, init);
 			break;
 		default:
 			throw new Error(`unknown block kind, ${tree.kind}`)
 	}
 	init(block)
+	return block;
+}
+
+function makeUnOpBlock(workspace, tree, init) {
+	let blockName;
+	switch (tree.symb) {
+		case '-': blockName = 'negate'; break;
+		default:
+			throw new Error(`unimplemented unary op: ${tree.symb}`)
+	}
+	const block = workspace.newBlock(`sheets_${blockName}`)
+	try {
+		const right = buildBlocks(workspace, tree.right, init)
+		if (right !== null) {
+			const conn = block.getInput('A').connection
+			conn.connect(right.outputConnection)
+		}
+	} catch(e) {
+		block.dispose()
+		throw e
+	}
+	return block;
+}
+
+function makeBinOpBlock(workspace, tree, init) {
+	let op, blockName;
+	switch (tree.symb) {
+		// arithmetic
+		case '+': blockName = 'arithmetic'; op = 'ADD'; break;
+		case '-': blockName = 'arithmetic'; op = 'MINUS'; break;
+		case '*': blockName = 'arithmetic'; op = 'MULTIPLY'; break;
+		case '/': blockName = 'arithmetic'; op = 'DIVIDE'; break;
+		case '^': blockName = 'arithmetic'; op = 'POWER'; break;
+		// compare
+		case '<': blockName = 'compare'; op = 'LT'; break;
+		case '<=': blockName = 'compare'; op = 'LTE'; break;
+		case '=': blockName = 'compare'; op = 'EQ'; break;
+		case '<>': blockName = 'compare'; op = 'NEQ'; break;
+		case '>': blockName = 'compare'; op = 'GT'; break;
+		case '>=': blockName = 'compare'; op = 'GTE'; break;
+		// concat TODO
+		case '&': blockName = 'join'; break;
+		default:
+			throw new Error(`unimplemented binary op: ${tree.symb}`)
+	}
+	const block = workspace.newBlock(`sheets_${blockName}`)
+	if (op) {
+		block.setFieldValue(op, 'OP')
+	}
+	try {
+		const left = buildBlocks(workspace, tree.left, init)
+		const right = buildBlocks(workspace, tree.right, init)
+		if (left !== null) {
+			const conn = block.getInput('A').connection
+			conn.connect(left.outputConnection)
+		}
+		if (right !== null) {
+			const conn = block.getInput('B').connection
+			conn.connect(right.outputConnection)
+		}
+	} catch(e) {
+		block.dispose()
+		throw e
+	}
+	return block;
+}
+
+// FIXME magic constant input_value
+// I think we can reference arg names(by index) directly instead now.
+const InputValue = 1
+function makeFuncBlock(workspace, tree, init) {
+	const block = workspace.newBlock(`sheets_${tree.name}`)
+	const inputs = block.inputList.filter(i => i.type === InputValue)
+	try {
+		if (tree.args.length > inputs.length) {
+			throw new Error(`too many arguments given (${tree.args.length}) for function ${tree.name} (wanted ${inputs.length})`)
+		}
+		tree.args.forEach((a, i) => {
+			const b2 = buildBlocks(workspace, a, init)
+			if (b2 !== null) {
+				const conn = inputs[i].connection
+				conn.connect(b2.outputConnection)
+			}
+		})
+	} catch(e) {
+		block.dispose()
+		throw e
+	}
+	return block;
+}
+
+function makeValueBlock(workspace, tree, init) {
+	let block = null;
+	switch (typeof tree.value) {
+		case 'number':
+			block = workspace.newBlock('sheets_number')
+			block.setFieldValue(tree.value, 'NUM')
+			break;
+		case 'string':
+			block = workspace.newBlock('sheets_text')
+			block.setFieldValue(tree.value, 'TEXT')
+			break;
+		case 'boolean':
+			block = workspace.newBlock('sheets_boolean')
+			block.setFieldValue(tree.value? 'TRUE' : 'FALSE', 'BOOL')
+			break;
+		default:
+			throw new Error("unimplemented value type")
+	}
 	return block;
 }
 
